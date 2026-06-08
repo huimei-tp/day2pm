df48fb2fbfd14998bf0da86e25287de2297658cb
huimei-tp <80399909+huimei-tp@users.noreply.github.com>
2026-06-08 20:45:14 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -10,4 +10,8 @@ const pool = mysql.createPool({
   connectionLimit: 10
 });
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+
 module.exports = pool;
