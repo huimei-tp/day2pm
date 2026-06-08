eaa9420aaca9fe3c6ae362d112bac9c879aa42b2
huimei-tp <80399909+huimei-tp@users.noreply.github.com>
2026-06-08 21:15:23 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,3 +1,8 @@
+
+
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -9,3 +14,6 @@ const pool = mysql.createPool({
     rejectUnauthorized: false
   }
 });
+
+
+module.exports = pool;
